dbfe07c573758b4846c8c65c75b1ed0e5e50998f
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2025-01-24 06:33:20 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -26,18 +26,13 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).then(response => {
-      if (!response || response.status !== 200 || response.type !== 'basic') {
-        return response;
-      }
-      const responseToCache = response.clone();
-      caches.open(CACHE_NAME).then(cache => {
-        cache.put(event.request, responseToCache);
-      });
-      return response;
-    }).catch(() => {
-      return caches.match(event.request);
-    })
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Return the cached response
+        }
+        return fetch(event.request); // Fetch from network
+      })
   );
 });
 
